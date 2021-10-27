79d2deed2142734bd97af790badbc78e5f757177
ZuevaMary <zuevamary94@gmail.com>
2021-10-27 21:09:49 +0300

    lesson-4

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', mainapp.main, name='index'),
     path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
-
+    path('auth/', include('authapp.urls', namespace='auth')),
     path('products/', include('mainapp.urls', namespace='products')),
 ]
 
